ffa5b90382987d78cb36fba1fffd62a2531d957e
futuretea <1913508671@qq.com>
2019-10-26 00:16:02 +0800

    update

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from flask import Flask
-from flask import make_response, request
+from flask import make_response, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    resp = make_response("helloworld")
-    resp.set_cookie('JSESSIONID', 'abcdef')
+    resp = make_response("root")
+    resp.set_cookie('JSESSIONID', 'root', path='/')
     return resp
 
+
+@app.route('/server1')
+def server1():
+    resp = make_response("sever1")
+    resp.set_cookie('JSESSIONID', 'server1', path='/server1')
+    return resp
+
+
+@app.route('/server2')
+def server2():
+    resp = make_response("server2")
+    resp.set_cookie('JSESSIONID', 'server2', path='/server2')
+    return resp
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=80)
